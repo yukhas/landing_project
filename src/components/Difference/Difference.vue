28b3d0b734b26b0912650945ec3b6f0834e15de1
Yuriy Borodachov <yuriy.borodachov@gmail.com>
2018-05-10 05:03:21 +0300

    added some styles to components

--- src/components/Difference/Difference.vue
+++ src/components/Difference/Difference.vue
@@ -5,5 +5,5 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped src="./Difference.scss">
+<style scoped src="./Difference.scss" lang="scss">
 </style>
