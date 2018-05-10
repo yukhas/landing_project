b152bc6f9556bee3178990d1ceca691762d6b5a8
Roman Dovhan <dovhan.rg@gmail.com>
2018-05-10 09:14:45 +0300

    Header

--- src/components/header/header.vue
+++
@@ -1,8 +0,0 @@
-<template src="./Header.html">
-</template>
-
-<script src="./Header.js">
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped src="./Header.scss"></style>
