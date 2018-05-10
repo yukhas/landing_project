e90d2e8ae574edad67a60f895822f79b65da99f3
Jenya <yevheniiah@onehourtranslation.com>
2018-05-10 03:00:50 +0300

    added middle part

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import Header from '../Header/Header.vue'
 import Switcher from '../Switcher/Switcher.vue'
 import Program from '../Program/Program.vue'
+import Middle from '../Middle/Middle.vue'
 
 export default {
     name: 'App',
     components: {
         HeaderApp: Header,
         Switcher: Switcher,
-        Program: Program
+        Program: Program,
+        Middle: Middle
     }
 }
